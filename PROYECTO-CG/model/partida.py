83eca43b60ecfce08202b8f9bbd0fce00d768ac9
Roddorr2 <150357479+Roddorr2@users.noreply.github.com>
2024-11-16 01:20:14 -0500

    Update partida.py
    Se agregó el atributo "clic" en el constructor de la clase Partida para manejar el conteo de clics al momento de jugar.

--- PROYECTO-CG/model/partida.py
+++ PROYECTO-CG/model/partida.py
@@ -1,16 +1,15 @@
 class Partida:
-    def __init__(self, idpartida, idusuario, idnivel, tiempo, fechapartida, resultado):
+    def __init__(self, idpartida, idusuario, idnivel, tiempo, fechapartida, resultado, clic):
         self.idpartida = idpartida
         self.idusuario = idusuario
         self.idnivel = idnivel
         self.tiempo = tiempo
         self.fechapartida = fechapartida
         self.resultado = resultado
-        
+        self.clic = clic  
+    
     def __str__(self):
-        return f"Partida: {self.idpartida}, Usuario: {self.idusuario}, Nivel: {self.idnivel}, Tiempo: {self.tiempo}, Fecha: {self.fechapartida}, Resultado: {self.resultado}"
-    
+        return f"Partida: {self.idpartida}, Usuario: {self.idusuario}, Nivel: {self.idnivel}, Tiempo: {self.tiempo}, Fecha: {self.fechapartida}, Resultado: {self.resultado}, Clics: {self.clic}"
+
     def __repr__(self):
-        return f"Partida(idpartida={self.idpartida}, idusuario={self.idusuario}, idnivel={self.idnivel}, tiempo={self.tiempo}, fechapartida='{self.fechapartida}', resultado='{self.resultado}')"
-    
-    
+        return f"Partida(idpartida={self.idpartida}, idusuario={self.idusuario}, idnivel={self.idnivel}, tiempo={self.tiempo}, fechapartida='{self.fechapartida}', resultado='{self.resultado}', clic={self.clic})"
