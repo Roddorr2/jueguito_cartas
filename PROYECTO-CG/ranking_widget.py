3fdec0c44340521108fc726b6eb2aead9dfcbe9e
Roddorr2 <150357479+Roddorr2@users.noreply.github.com>
2024-11-16 03:47:37 -0500

    Update ranking_widget.py
    Se le agrega una columna extra al ranking para ver los clics

--- PROYECTO-CG/ranking_widget.py
+++ PROYECTO-CG/ranking_widget.py
@@ -4,26 +4,27 @@ class RankingWidget(QWidget):
     def __init__(self, top_10_data):
         super().__init__()
         self.setWindowTitle("Ranking de Jugadores")
-        self.setGeometry(300, 300, 450, 350)
-        self.setMinimumSize(450, 350)  # Tamaño mínimo de ventana
+        self.setGeometry(300, 300, 600, 400)
+        self.setMinimumSize(600, 400)
         
         # Crear layout
         layout = QVBoxLayout()
 
         # Crear la tabla
         self.table_widget = QTableWidget()
-        self.table_widget.setColumnCount(4)  # Número de columnas
-        self.table_widget.setHorizontalHeaderLabels(["Username", "Descripción", "Tiempo", "Fecha de Partida"])
+        self.table_widget.setColumnCount(5)  # Número de columnas
+        self.table_widget.setHorizontalHeaderLabels(["Username", "Descripción", "Tiempo", "Fecha de Partida", "Clics"])
         
         # Ajustar el tamaño de la tabla
         self.table_widget.setRowCount(len(top_10_data))
         
         # Llenar la tabla con los datos de la lista
-        for row, (username, descripcion, tiempo, fechapartida) in enumerate(top_10_data):
+        for row, (username, descripcion, tiempo, fechapartida, clic) in enumerate(top_10_data):
             self.table_widget.setItem(row, 0, QTableWidgetItem(username))
             self.table_widget.setItem(row, 1, QTableWidgetItem(descripcion))
             self.table_widget.setItem(row, 2, QTableWidgetItem(tiempo))
             self.table_widget.setItem(row, 3, QTableWidgetItem(fechapartida))
+            self.table_widget.setItem(row, 4, QTableWidgetItem(str(clic)))
 
         # Agregar la tabla al layout
         layout.addWidget(self.table_widget)
